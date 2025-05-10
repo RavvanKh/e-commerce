28cae4cecb4612b1f3366b973f512aa5fae8e956
Ravann <revann.gitlab@gmail.com>
2025-05-10 16:51:40 +0400

    test

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -69,7 +69,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://e-commerce-api-red.vercel.app/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -124,7 +124,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "http://localhost:5000/api/auth/check-auth",
+      "https://e-commerce-api-red.vercel.app/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://e-commerce-ravankhaligov.vercel.app"],
+    origin: ["https://e-commerce-api-ravankhaligov.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
