dddf291d9fbbdbe5b41892e521103db1711d3834
Ravann <revann.gitlab@gmail.com>
2025-05-10 20:55:38 +0400

    main

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,17 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src'),
+    }
   },
   build: {
-    outDir: "dist", // Vercel'in otomatik tanıdığı klasör
-    sourcemap: true, // Production'da hata ayıklama
-  },
-  base: "/", // Vercel'de root domain için
-});
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  }
+})
